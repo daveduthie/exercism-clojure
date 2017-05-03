cbb423bd36fbb88920b07c94ddd8dac46d548f2c
David Duthie <dave.a.duthie@gmail.com>
2017-05-03 19:18:27 +0800

    simplify raindrop

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,50 +1,13 @@
-fn factor(number: i32) -> Vec<i32> {
-    let mut number = number;
-    let mut factors = vec![];
-    let mut candidate = 2;
-
-    while number > 1 {
-        while number % candidate == 0 {
-            factors.push(candidate);
-            number /= candidate;
-        }
-        candidate += 1;
-    }
-
-    factors
-}
-
 pub fn raindrops(number: i32) -> String {
-    let factors = factor(number);
-
-    let mut pling: bool = false;
-    let mut plang: bool = false;
-    let mut plong: bool = false;
-
-    for n in factors {
-        match n {
-            3 => pling = true,
-            5 => plang = true,
-            7 => plong = true,
-            _ => (),
-        }
-    }
-
     let mut output = String::new();
-    if pling {
-        output += "Pling";
-    }
-    if plang {
-        output += "Plang";
-    }
-    if plong {
-        output += "Plong";
-    }
+    
+    if number % 3 == 0 {output += "Pling"}
+    if number % 5 == 0 {output += "Plang"}
+    if number % 7 == 0 {output += "Plong"}
 
     if output.is_empty() {
         number.to_string()
     } else {
         output
     }
-
 }
