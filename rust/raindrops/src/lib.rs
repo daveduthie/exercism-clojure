f08244f36550ceb5b4b763b2741de23aa0a696bf
David Duthie <dave.a.duthie@gmail.com>
2017-05-05 22:33:43 +0800

    rustfmt ... :(

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,9 +1,15 @@
 pub fn raindrops(number: i32) -> String {
     let mut output = String::new();
-    
-    if number % 3 == 0 {output += "Pling"}
-    if number % 5 == 0 {output += "Plang"}
-    if number % 7 == 0 {output += "Plong"}
+
+    if number % 3 == 0 {
+        output += "Pling"
+    }
+    if number % 5 == 0 {
+        output += "Plang"
+    }
+    if number % 7 == 0 {
+        output += "Plong"
+    }
 
     if output.is_empty() {
         number.to_string()
