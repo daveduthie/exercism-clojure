607d5a3afcdd8f21c930b2504638ffd9c3a936f0
David Duthie <dave.a.duthie@gmail.com>
2017-05-17 20:09:05 +0800

    rust beer song v3

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,35 +1,40 @@
-pub fn verse<'a>(num: u32) -> String {
-    let (num_bef, units_bef, num_aft, units_aft, det) = match num {
-        1 | 0 => ("1".to_string(), "bottle", "no more".to_string(), "bottles", "it"),
-        _ => {
-            (num.to_string(),
-             "bottles",
-             (num - 1).to_string(),
-             if num == 2 { "bottle" } else { "bottles" },
-             "one")
-        }
-    };
-
+fn unit(num: u32, capitalise: bool) -> String {
     match num {
+        0 if capitalise => "No more bottles".to_string(),
+        0 => "no more bottles".to_string(),
+        1 => "1 bottle".to_string(),
+        _ => num.to_string() + " bottles",
+    }
+}
+
+fn determiner(num: u32) -> String {
+    match num {
+        1 => "it".to_string(),
+        _ => "one".to_string(),
+    }
+}
+
+pub fn verse(num: u32) -> String {
+    let prefix = format!("{} of beer on the wall, \
+                          {} of beer.\n",
+                         unit(num, true),
+                         unit(num, false));
+
+    let suffix = match num {
         0 => {
-            "No more bottles of beer on the wall, \
-             no more bottles of beer.\n\
-             Go to the store and buy some more, \
-             99 bottles of beer on the wall.\n"
+            "Go to the store and buy some more, \
+              99 bottles of beer on the wall.\n"
                     .to_string()
         }
         _ => {
-            format!("{nb} {ub} of beer on the wall, \
-                     {nb} {ub} of beer.\n\
-                     Take {det} down and pass it around, \
-                     {na} {ua} of beer on the wall.\n",
-                    nb = num_bef,
-                    ub = units_bef,
-                    det = det,
-                    na = num_aft,
-                    ua = units_aft)
+            format!("Take {} down and pass it around, \
+                      {} of beer on the wall.\n",
+                    determiner(num),
+                    unit(num - 1, false))
         }
-    }
+    };
+
+    prefix + &suffix
 }
 
 pub fn sing(high: u32, low: u32) -> String {
