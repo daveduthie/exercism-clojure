f187a6f20bf66adcfb3e46b2607a8986d255e83a
David Duthie <dave.a.duthie@gmail.com>
2017-05-18 15:09:58 +0800

    update with formulae which are more fun (but less clear)

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,17 +1,19 @@
+/// Borrowing from @schodet and Gauss
 pub fn square_of_sum(n: u64) -> u64 {
-    let mut acc: u64 = 0;
-    for x in 1..(n + 1) {
-        acc += x;
-    }
+    let acc = (n + 1) * n / 2;
     acc * acc
 }
 
+/// Borrowing from @schodet again, as well as from:
+/// https://trans4mind.com/personal_development/mathematics/series/sumNaturalSquares.htm
+///
+/// `sum_of_squares = (n^3)/3 + (n^2)/2 + n/6`
+/// or `(2n^3 + 3n^2 + n) / 6`
 pub fn sum_of_squares(n: u64) -> u64 {
-    let mut acc: u64 = 0;
-    for x in 1..(n + 1) {
-        acc += x * x;
-    }
-    acc
+    let cubic_term = 2 * n * n * n;
+    let square_term = 3 * n * n;
+    let linear_term = n;
+    (cubic_term + square_term + linear_term) / 6
 }
 
 pub fn difference(n: u64) -> u64 {
