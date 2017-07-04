736c2e6388d57a4f3e159313ccc795d36dba2380
David Duthie <dave.a.duthie@gmail.com>
2017-07-04 20:34:35 +0800

    use java.util.Calendar for calculation

--- clojure/gigasecond/src/gigasecond.clj
+++ clojure/gigasecond/src/gigasecond.clj
@@ -1,43 +1,17 @@
-(ns gigasecond)
+(ns gigasecond
+  (:require [clojure.string :as str])
+  (:import java.util.Calendar))
 
-(defn- leap-year? [y]
-  (and (zero? (mod y 4))
-       (or (not= 0 (mod y 100))
-           (zero? (mod y 400)))))
+(defn from [yyyy mm dd]
+  (let [g 1000000000
+        d (doto (Calendar/getInstance)
+            (.set yyyy (dec mm) dd)
+            (.add Calendar/SECOND g))
+        yyyy' (.get d Calendar/YEAR)
+        mm' (inc (.get d Calendar/MONTH))
+        dd' (.get d Calendar/DATE)]
+    [yyyy' mm' dd']))
 
-(defn- month-length [month year]
-  (condp = month
-    1  31
-    2  (if (leap-year? year) 29 28)
-    2  28
-    3  31
-    4  30
-    5  31
-    6  30
-    7  31
-    8  31
-    9  30
-    10 31
-    11 30
-    0  31)) ; because (mod 12 12) is 0
-
-(def days-in-gs
-  (quot 1000000000 (* 60 60 24)))
-
-(defn from [year month day]
-  ;; set a preposterous date
-  (let [day' (+ day days-in-gs)]
-    ;; loop until we balance things out
-    (loop [y year m month d day']
-      (let [max-d (month-length (rem m 12) y)]
-        (cond
-
-          ;; it's important to increment year each time we roll over 12 months,
-          ;; otherwise we get errors when trying to figure out if it's a leap year.
-          (> m 12)
-          (recur (+ y (quot m 12)) (rem m 12) d)
-
-          (> d max-d)
-          (recur y (inc m) (- d max-d))
-
-          :else [y m d])))))
+;; (add-time 2017 07 04)
+;; (add-time 2011 4 25)
+;; (add-time 1977 6 13)
